2bbcc161629fb5365b22a080a10b29aea373c20d
Candido Hernandez <candido.hern88@gmail.com>
2019-08-18 14:56:00 -0400

    Refactor views using viewsets

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,12 +1,38 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from snippets import views
+from snippets.views import SnippetViewSet, UserViewSet, api_root
 
-urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippet/<int:pk>', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('user/<int:pk>', views.UserDetail.as_view()),
-]
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'list',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+snippet_highlight = SnippetViewSet({
+    'get': 'highlight'
+})
+
+user_list = UserViewSet({
+    'get': 'list'
+})
+
+user_detail = UserViewSet({
+    'get': 'retrieve'
+})
+
+urlpatterns = format_suffix_patterns([
+    path('', views.api_root),
+    path('snippets/', snippet_list, name='snippet_list'),
+    path('snippets/<int:pk>', snippet_detail, name='snippet_detail'),
+    path(
+        'snippets/<int:pk>/highlight', snippet_highlight, name='snippet_highlight'
+    ),
+    path('users/', user_list, name='user_list'),
+    path('users/<int:pk>', user_detail, name='user_detail'),
+])
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('snippets.urls')),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
